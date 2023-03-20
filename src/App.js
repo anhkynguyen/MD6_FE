9a58d59cbbb8d6c7518cecf975239d3dadb6e42c
dangtung <tungdang1997@example.com>
2023-03-20 10:26:32 +0700

    newAdd

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ChangePassword from "./pages/user/changePassword";
 import EditProvider from "./pages/provider/editProvider";
 import AddProvider from "./pages/provider/addProvider";
 import RegisterTest from "./pages/user/register";
+import NewAddProvider from "./pages/provider/newAddProvider";
 
 function App() {
   const user = useSelector((state) => state.user.currentUser);
@@ -30,7 +31,7 @@ function App() {
             <>
               <Route path={"home"} element={<Home />}>
                 <Route path={""} element={<ListProvider />}></Route>
-                <Route path={"add-post"} element={<AddProvider />} />
+                <Route path={"newAdd-post"} element={<NewAddProvider />} />
                 <Route path={"edit-post/:id"} element={<EditProvider />} />
               </Route>
               <Route path={"user"} element={<User />}>
